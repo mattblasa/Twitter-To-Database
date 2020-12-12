2e83f09bb7369a9307a69106b71ca0aae65afa3e
Matthew_Blasa <blasa.matthew@yahoo.com>
2020-12-12 10:32:30 -0600

    Update Scraping_Script_Main.py

--- Scraping_Script_Main.py
+++ Scraping_Script_Main.py
@@ -24,30 +24,30 @@ class auth:
 
     def extract_tweet_attributes(tweet_object):
         # create empty list
-    tweet_list =[]
-    # loop through tweet objects
-    for tweet in tweet_object:
-        tweet_id = tweet.id # unique integer identifier for tweet
-        text = tweet.text # utf-8 text of tweet
-        favorite_count = tweet.favorite_count
-        retweet_count = tweet.retweet_count
-        created_at = tweet.created_at # utc time tweet created
-        source = tweet.source # utility used to post tweet
-        reply_to_status = tweet.in_reply_to_status_id # if reply int of orginal tweet id
-        reply_to_user = tweet.in_reply_to_screen_name # if reply original tweets screenname
-        retweets = tweet.retweet_count # number of times this tweet retweeted
-        favorites = tweet.favorite_count # number of time this tweet liked
-        # append attributes to list
-        tweet_list.append({'tweet_id':tweet_id, 
-                          'text':text, 
-                          'favorite_count':favorite_count,
-                          'retweet_count':retweet_count,
-                          'created_at':created_at, 
-                          'source':source, 
-                          'reply_to_status':reply_to_status, 
-                          'reply_to_user':reply_to_user,
-                          'retweets':retweets,
-                          'favorites':favorites})
-    # create dataframe   
-    df = pd.DataFrame(tweet_list) #removed extra columns, which are already rendered 
-    return df
+        tweet_list =[]
+        # loop through tweet objects
+        for tweet in tweet_object:
+            tweet_id = tweet.id # unique integer identifier for tweet
+            text = tweet.text # utf-8 text of tweet
+            favorite_count = tweet.favorite_count
+            retweet_count = tweet.retweet_count
+            created_at = tweet.created_at # utc time tweet created
+            source = tweet.source # utility used to post tweet
+            reply_to_status = tweet.in_reply_to_status_id # if reply int of orginal tweet id
+            reply_to_user = tweet.in_reply_to_screen_name # if reply original tweets screenname
+            retweets = tweet.retweet_count # number of times this tweet retweeted
+            favorites = tweet.favorite_count # number of time this tweet liked
+            # append attributes to list
+            tweet_list.append({'tweet_id':tweet_id, 
+                            'text':text, 
+                            'favorite_count':favorite_count,
+                            'retweet_count':retweet_count,
+                            'created_at':created_at, 
+                            'source':source, 
+                            'reply_to_status':reply_to_status, 
+                            'reply_to_user':reply_to_user,
+                            'retweets':retweets,
+                            'favorites':favorites})
+        # create dataframe   
+        df = pd.DataFrame(tweet_list) #removed extra columns, which are already rendered 
+        return df
